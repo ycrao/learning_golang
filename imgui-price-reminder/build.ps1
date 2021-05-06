3c7149d68ffbd84e1def94fa28c56ddb41abc926
raoyc <raoyc2009@gmail.com>
2021-05-06 18:18:46 +0800

    build price-reminder in static mode

--- imgui-price-reminder/build.ps1
+++ imgui-price-reminder/build.ps1
@@ -1,1 +1,1 @@
-$env:GOOS='windows'; $env:GOARCH='amd64'; go build -ldflags "-s -w -H=windowsgui" -o PriceReminder.exe; $env:GOGS=''; $env:GOARCH='';
+$env:GOOS='windows'; $env:GOARCH='amd64'; go build -ldflags "-extldflags '-static' -s -w -H=windowsgui" -o PriceReminder.exe; $env:GOGS=''; $env:GOARCH='';
--- imgui-price-reminder/go.mod
+++ imgui-price-reminder/go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/AllenDang/giu v0.4.2
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20210410170116-ea3d685f79fb // indirect
 	github.com/go-resty/resty/v2 v2.3.0
 	github.com/joho/godotenv v1.3.0
 	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966
